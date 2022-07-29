90c71ebd83ba78212cf2897c220b98acd9193954
Deep Hansda <deephansda921@gmail.com>
2022-07-29 19:33:19 +0530

    little changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const PORT = 3400 || process.env.PORT;
+const PORT = process.env.PORT || 3400;
 
 const visitorRouter = require('./src/routes/visitorsRoutes')
 const appsRouter = require('./src/routes/appsRoutes')
@@ -13,17 +13,17 @@ app.use(express.json())
 app.use(cors())
 
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.status(200).json({
         message: 'hello world'
     })
 })
 
-app.use('/api',visitorRouter)
-app.use('/api',appsRouter)
-app.use('/api',adsRouter)
+app.use('/api', visitorRouter)
+app.use('/api', appsRouter)
+app.use('/api', adsRouter)
 
 
-app.listen(PORT,()=>{
-    console.log('listening on port ' +PORT)
+app.listen(PORT, () => {
+    console.log('listening on port ' + PORT)
 })
